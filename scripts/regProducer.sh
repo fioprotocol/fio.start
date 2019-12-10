d50f84322f3f989df007af474c8ddfe82445e92d
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2019-12-11 01:33:33 +0200

    Update regProducer.sh

--- scripts/regProducer.sh
+++ scripts/regProducer.sh
@@ -9,12 +9,13 @@
 ###############################################################################
 
 fioAddress="cryptolions:fiotestnet"
+signkey="FIO6dibuddHN5fioh3VczFXWJq4M3quFjvXoRKR5S9BwgxLNaYf5L"
 actor=5tyuyv241iup
 url="bpurl.io"
 location=10
 maxfee=40000000000
 
-./cleos.sh push action eosio regproducer '{"fio_address":"'$fioAddress'","url":"'$url'","location":'$location',"actor":"'$actor'", "max_fee":"'$maxfee'"}' --permission $actor@active
+./cleos.sh push action eosio regproducer '{"fio_address":"'$fioAddress'","owner_fio_public_key":"'$signkey'","url":"'$url'","location":'$location',"actor":"'$actor'", "max_fee":"'$maxfee'"}' --permission $actor@active
 
 #Example
 #./cleos.sh push action eosio regproducer '{"fio_address":"cryptolions:fiotestnet","url":"CryptoLions.io","location":10,"actor":"5tyuyv241iup", "max_fee":"40000000000"}' --permission 5tyuyv241iup@active
