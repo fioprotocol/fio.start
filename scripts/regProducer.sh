10b3e65da290155c8fe3273d07047c13740a5b5d
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2019-12-17 00:48:50 +0200

    Update regProducer.sh

--- scripts/regProducer.sh
+++ scripts/regProducer.sh
@@ -12,12 +12,22 @@
 # To convert key to actor name you can use clio:
 #./clio.sh convert fiokey_to_account FIO6dibuddHN5fioh3VczFXWJq4M3quFjvXoRKR5S9BwgxLNaYf5L
 
+#Locations
+#10  East Asia
+#20  Australia
+#30  West Asia
+#40  Africa
+#50  Europe
+#60  East North America
+#70  South America
+#80  West North America
+
 fioAddress="cryptolions:fiotestnet"
 actor=5tyuyv241iup
 
 signkey="FIO5J3K9zd6e9u4d1whTPx1jUZ1cdJqSvHqcKVYma9wZdfyfaFiob"
 url="bpurl.io"
-location=10
+location=50
 maxfee=40000000000
 
 ./clio.sh push action eosio regproducer '{"fio_address":"'$fioAddress'","fio_pub_key":"'$signkey'","url":"'$url'","location":'$location',"actor":"'$actor'", "max_fee":"'$maxfee'"}' --permission $actor@active
