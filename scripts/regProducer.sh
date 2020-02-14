92de4d6b34524ecdd8f90590c1f4e9f826feb279
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2020-02-14 19:32:29 +0000

    Update regProducer.sh

--- scripts/regProducer.sh
+++ scripts/regProducer.sh
@@ -28,10 +28,10 @@ actor=5tyuyv241iup
 signkey="FIO5J3K9zd6e9u4d1whTPx1jUZ1cdJqSvHqcKVYma9wZdfyfaFiob"
 url="bpurl.io"
 location=50
-maxfee=40000000000
+maxfee=200000000000
 
 ./clio.sh push action eosio regproducer '{"fio_address":"'$fioAddress'","fio_pub_key":"'$signkey'","url":"'$url'","location":'$location',"actor":"'$actor'", "max_fee":"'$maxfee'"}' --permission $actor@active
 
 #Example
-#./clio.sh push action eosio regproducer '{"fio_address":"cryptolions@fiotestnet","fio_pub_key":"FIO6dibuddHN5fioh3VczFXWJq4M3quFjvXoRKR5S9BwgxLNaYf5L","url":"CryptoLions.io","location":10,"actor":"5tyuyv241iup", "max_fee":"40000000000"}' --permission 5tyuyv241iup@active
+#./clio.sh push action eosio regproducer '{"fio_address":"cryptolions@fiotestnet","fio_pub_key":"FIO6dibuddHN5fioh3VczFXWJq4M3quFjvXoRKR5S9BwgxLNaYf5L","url":"CryptoLions.io","location":10,"actor":"5tyuyv241iup", "max_fee":"200000000000"}' --permission 5tyuyv241iup@active
 
